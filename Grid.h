2529d34b5a4b661100f31fee4f94850a43a95813
Vishal <vshldc@gmail.com>
2015-02-09 04:27:55 -0500

    First and last commit

---
+++ Grid.h
@@ -0,0 +1,17 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Vishal Chandnani on 2/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+- (void)evolveStep;
+- (void)countNeighbors;
+- (void)updateCreatures;
+@end
